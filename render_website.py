9dfff8b53dc3040453b941d3b074dd8802bafff7
cok263 <cok26385@gmail.com>
2020-12-03 00:20:21 +0300

    Display books genres

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import os
 import json
+import math
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from livereload import Server
@@ -18,9 +19,11 @@ def on_reload():
     with open('download/books.json', 'r') as lib_file:
         books_json = lib_file.read()
     books = json.loads(books_json)
-    for num_page, books_page in enumerate(list(chunked(books, 20))):
+    for num_page, books_page in enumerate(list(chunked(books, 10)), 1):
         rendered_page = template.render(
             books=list(chunked(books_page, 2)),
+            pages_count=math.ceil(len(books) / 10),
+            current_page=num_page,
         )
         with open(f'pages/index{num_page}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
