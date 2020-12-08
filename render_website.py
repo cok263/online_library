ce273cfd3aae33b668177d8473a127ef4b47ed19
cok263 <cok26385@gmail.com>
2020-12-08 21:32:10 +0300

    add clean pages function

--- render_website.py
+++ render_website.py
@@ -1,6 +1,7 @@
 import os
 import json
 import math
+from glob import glob
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from livereload import Server
@@ -9,6 +10,19 @@ from more_itertools import chunked
 
 BOOKS_ON_PAGE_COUNT = 10
 COLUMN_BOOKS_COUNT = 2
+
+
+def cleanPages(pages_count):
+    files_in_pages = set(glob('pages/*.html'))
+    expected_pages = {f'pages/index{i}.html'
+                      for i in range(1, pages_count + 1)}
+    old_pages = files_in_pages - expected_pages
+
+    for file in old_pages:
+        try:
+            os.remove(file)
+        except:
+            print(f'File {file} can not be removed')
 
 
 def on_reload():
@@ -23,10 +37,14 @@ def on_reload():
     with open('download/books.json', 'r') as lib_file:
         books_json = lib_file.read()
     books = json.loads(books_json)
-    for num_page, books_page in enumerate(list(chunked(books, BOOKS_ON_PAGE_COUNT)), 1):
+    pages_count = math.ceil(len(books) / BOOKS_ON_PAGE_COUNT)
+    books_on_pages = list(chunked(books, BOOKS_ON_PAGE_COUNT))
+
+    for num_page, books_page in enumerate(books_on_pages, 1):
+
         rendered_page = template.render(
             books=list(chunked(books_page, COLUMN_BOOKS_COUNT)),
-            pages_count=math.ceil(len(books) / BOOKS_ON_PAGE_COUNT),
+            pages_count=pages_count,
             current_page=num_page,
         )
         with open(
@@ -34,6 +52,7 @@ def on_reload():
         ) as file:
             file.write(rendered_page)
         print('template reloaded')
+    cleanPages(pages_count)
 
 
 def main():
